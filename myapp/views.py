bb85205f2799349ba6053c746b73dc04eeda24e9
1999rahulreddy <1999rahulreddy@gmail.com>
2023-10-02 04:17:36 -0700

    first build

---
+++ myapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'signup.html'
+    success_url = 'login'
