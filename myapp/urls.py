e729a8d71298b184983ba037e59b6ec78811754c
1999rahulreddy <1999rahulreddy@gmail.com>
2023-10-04 21:27:28 -0700

    push

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from .views import SignUpView
 from . import views
 
 urlpatterns = [
-    path('signup/', views.SignUpView.as_view(), name='signup'),
+    #path('signup', views.SignUpView.as_view(), name='signup'),
+    path('signup/', views.SignUpView, name='signup'),
+    path('', views.home, name='home',)
     # Add other URLs as needed
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 
+'''
 class SignUpView(CreateView):
     form_class = UserCreationForm
     template_name = 'signup.html'
     success_url = 'login'
+    def get(self, request):
+        return render(CreateView, 'signup.html', {})
+'''
+
+def SignUpView(request):
+    return render(request, 'Signup.html', {})
+def home(request):
+    return render(request, 'home.html', {})
