f58e5882434b89bd6ff234fe490f4a93130b7fb9
1999rahulreddy <1999rahulreddy@gmail.com>
2023-10-05 13:08:09 -0700

    Added code upload and output display feature

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     #path('signup', views.SignUpView.as_view(), name='signup'),
     path('signup/', views.SignUpView, name='signup'),
-    path('', views.home, name='home',)
+    #path('', views.home, name='home',),
+    #path('upload', views.upload_file, name='upload'),
+    path('upload/', views.upload_script, name='upload-script'),
     # Add other URLs as needed
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
+#from django.views.decorators.csrf import csrf_exempt
+from pathlib import Path
+import os, subprocess
 
 '''
 class SignUpView(CreateView):
@@ -15,3 +18,21 @@ def SignUpView(request):
     return render(request, 'Signup.html', {})
 def home(request):
     return render(request, 'home.html', {})
+def upload_script(request):
+    if request.method == 'POST':
+        uploaded_file = request.FILES['file']
+        BASE_DIR = Path(__file__).resolve().parent
+        file_path = os.path.join(BASE_DIR, 'uploadedcode/a.py')
+        with open(file_path, 'wb+') as f:
+            for chunk in uploaded_file.chunks():
+                f.write(chunk)
+
+        # Execute the Python script using subprocess
+        result = subprocess.run(['python', file_path], capture_output=True, text=True)
+        
+        # Access the script output using 'result.stdout'
+        script_output = result.stdout
+
+        return render(request, 'output.html', {'output': script_output})
+    
+    return render(request, 'upload.html')
